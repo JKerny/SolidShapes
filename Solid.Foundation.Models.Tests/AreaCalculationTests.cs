bcc5eda8730203a69aee7ae8e6b840486302bb91
Jeremy Kern <jkerny81@gmail.com>
2018-10-06 14:51:50 +1000

    Added unit test method to check rectangle area method

---
+++ Solid.Foundation.Models.Tests/AreaCalculationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Solid.Foundation.Models.Tests
+{
+    public class AreaCalculationTests
+    {
+        [Fact]
+        public void RectangleAreaCalculationTest()
+        {
+            //arrange
+            var expected = 8;
+
+            //act
+            var rectangle = new Rectangle();
+            rectangle.Length = 4;
+            rectangle.Width = 2;
+            var actual = rectangle.Area();
+
+            //assert
+            Assert.Equal(actual, expected);
+            
+        }
+    }
+}
