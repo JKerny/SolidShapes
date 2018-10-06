c4d58ed4e56f47d799fd3df6235ae34ab52167de
Jeremy Kern <jkerny81@gmail.com>
2018-10-06 18:10:48 +1000

    added test for circle

--- Solid.Foundation.Models.Tests/AreaCalculationTests.cs
+++ Solid.Foundation.Models.Tests/AreaCalculationTests.cs
@@ -24,5 +24,19 @@ namespace Solid.Foundation.Models.Tests
             Assert.Equal(actual, expected);
             
         }
+
+        [Theory]
+        [InlineData(6,113.1)]
+        public void CircleAreaCalculation (double radius,double expected)
+        {
+            //act
+            var circle = new Circle();
+            circle.Radius = radius;
+            var actual = Math.Round(circle.Area(),1);
+
+            //assert
+            Assert.Equal(expected, actual);
+
+        }
     }
 }
