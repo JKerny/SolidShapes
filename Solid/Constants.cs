5ac2847edcc77ddb84545dd1b5c7bf8d9c62d8a1
Jeremy Kern <jkerny81@gmail.com>
2018-10-07 21:25:05 +1100

    Changed program to work with input from console

---
+++ Solid/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid
+{
+    public static class Constants
+    {
+        public const string EnterShapeName = "Please enter a shape to calculate the area of Squrea(s), Rectangle(R), Triangle(t)"; 
+    }
+}
--- Solid/ProgramService.cs
+++ Solid/ProgramService.cs
@@ -1,5 +1,9 @@
 ﻿using Solid.Foundation.Repository;
 using System;
+using Solid.Foundation.Models;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Solid
 {
@@ -18,12 +22,42 @@ namespace Solid
 
         public void Execute()
         {
-            var shapes = _shapeRepository.GetShapes();
-            foreach(var shape in shapes)
-            {
-                Console.WriteLine(shape.Area());
+            Console.WriteLine(Constants.EnterShapeName);            
+            var shape = DetermineShape(Console.ReadLine());
+            if(shape != null)
+            {                
+                foreach(var property in GetProperties(shape))
+                {
+                    Console.WriteLine("Please enter a " + property);
+                    Console.ReadLine();
+                }
             }
-            Console.ReadLine();
+        }
+
+        private List<string> GetProperties(object shape)
+        {
+            List<string> properties = new List<string>();
+            Type t = shape.GetType();
+            PropertyInfo[] props = t.GetProperties();            
+            foreach (PropertyInfo property in props)
+            {
+                properties.Add(property.Name);
+            }
+            return properties;
+        }
+
+        private IShape DetermineShape(string input)
+        {            
+            var shapeAssembly = typeof(IShape).Assembly;
+            var shapeType = shapeAssembly.GetTypes().Where(t => t.GetInterface("IShape") == typeof(IShape));
+            var type = shapeType.Where(x => x.Name.ToLower()[0] == input.ToLower()[0]).FirstOrDefault();
+            var shape = Activator.CreateInstance(shapeAssembly.FullName, type.FullName);
+            return (IShape)shape.Unwrap();
+        }
+
+        public static T Cast<T>(object o)
+        {
+            return (T)o;
         }
     }
 }
